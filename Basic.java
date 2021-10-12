230dbdaec72ac1d5955e04245c779ddf235bd10a
aissyyy <91513350+aissyyy@users.noreply.github.com>
2021-10-12 13:24:13 +0700

    Add files via upload

---
+++ Basic.java
@@ -0,0 +1,7 @@
+public class Basic{
+    public static void main (String[] args){
+    String firstName = "Aisyah" ; // 1
+    int number = 1234;
+    System.out.println(firstName + number);
+    }
+}
---
+++ Challenge.java
@@ -0,0 +1,28 @@
+public class Challenge {
+     public static void main(String[] args) {
+        // leave comment in here
+        // int integerNumber;
+        // integerNumber;
+        // system.out.println(integerNumber);
+        // string firstName = "Aisyah";
+        // string lastName = "Nabillah";
+        // int number = 2000;
+        // double dollar = 10.10;
+        // System.out.println(firstName + number);
+        String firstName = "Aisyah";
+        String lastName = "Nabillah";
+        int age = 21;
+        String address = " Nongsa,Batam ";
+        String number = "085830010089";
+        String hobi = "cooking";
+        String makes = "Nasi Goreng";
+        System.out.println("first name :" + firstName);
+        System.out.println("last name :" + lastName);
+        System.out.println("Age :" + age);
+        System.out.println("Address :" + address);
+        System.out.println("Phone number :" + number);
+        System.out.println("hobi : " + hobi);
+        System.out.println("makanan kesukaan : " + makes);
+
+     }
+    }
